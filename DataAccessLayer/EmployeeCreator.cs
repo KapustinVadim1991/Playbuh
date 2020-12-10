0923c7ad944976d9eb59d42abc0af57c34ac41d7
KapustinVadim1991 <kapustin199123@yandex.ru>
2020-12-10 22:49:45 +0500

    First Commit

---
+++ DataAccessLayer/EmployeeCreator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class EmployeeCreator
+    {
+
+        public void AddNewEmploye(string fio)
+        {
+            using(PLAYBUHEntities context = new PLAYBUHEntities())
+            {
+                context.EMPLOYEES.Add(
+                    new EMPLOYEES { FIO = fio }
+                    );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
